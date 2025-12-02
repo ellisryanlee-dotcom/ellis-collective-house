802c69db7ebc5c792f55fb980f690fdb22d8b8f5
ellisryanlee-dotcom <ellis.ryanlee@gmail.com>
2025-12-02 02:14:10 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-iimport "./globals.css";
+import "./globals.css";
 import { Inter_Tight } from "next/font/google";
 import type { Metadata } from "next";
 
 const interTight = Inter_Tight({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"]
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
   description: "110 Production digital home",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={interTight.className}>
