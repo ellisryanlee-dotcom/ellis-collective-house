045d298f43d6f3f256d994a1909e609334328fd5
ellisryanlee-dotcom <ellis.ryanlee@gmail.com>
2025-12-02 02:39:10 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      {/* your content here */}
+    <main className="min-h-screen flex items-center justify-center bg-black text-white text-4xl">
+      110 Productions
     </main>
   );
 }
